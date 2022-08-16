266ba891420d8c3d84815204be62029b38e06bd3
DonOutcast <sham1996@yandex.ru>
2022-08-16 14:30:05 +0300

    see add

--- src/databases/sql_database.py
+++ src/databases/sql_database.py
@@ -8,10 +8,23 @@ class DatabaseBot:
         self.base = sq.connect(f"{self.name}")
         self.cur = self.base.cursor()
 
-    def sql_start(self):
+    def sql_create_users(self):
         if self.base:
             print("Data base connected OK!")
-        self.cur.execute("""CREATE TABLE IS NOT EXISTS users(id INTEGER PRIMARY KEY, login VARCHAR(20), role VARCHAR(20),
-                          campus Varchar(20);""")
+            self.cur.execute("""CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, login VARCHAR(20), role VARCHAR(20), campus Varchar(20))""")
+            self.base.commit()
 
-        self.cur.commit()
+    def sql_create_booking(self):
+        if self.base:
+            print("Data base connected OK!")
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS booking(start_time DATE, end_time DATE, status INTEGER, user_id INTEGER, object_id INTEGER)""")
+        self.base.commit()
+
+    def sql_create_objects(self):
+        if self.base:
+            print("Data base connected OK!")
+        self.cur.execute("""CREATE TABLE IF NOT EXISTS objects(id INTEGER PRIMARY KEY, name VARCHAR(30), type VARCHAR(20), description VARCHAR(50), campus VARCHAR(20), floor INTEGER, number_of_the_room INTEGER, image TEXT)""")
+        self.base.commit()
+
+    def add_user(self, user_id, login, role, campus):
+        self.cur.execute("""INSERT INTO user VALUES(?, ?, ?, ?);, ()""")
--- src/main.py
+++ src/main.py
@@ -9,8 +9,10 @@ async def on_starttup(_):
 
 
 system_commands.register_handlers_system(dp)
-a = sql_database.DatabaseBot("biil.db")
-a.sql_start()
+a = sql_database.DatabaseBot("data_bot.db")
+a.sql_create_users()
+a.sql_create_booking()
+a.sql_create_objects()
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True, on_startup=on_starttup)
